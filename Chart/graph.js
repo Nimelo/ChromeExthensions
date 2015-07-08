369015d442d103e20bab96eaf3e035aeba0da750
Mateusz Gąsior <mr.nimelo@gmail.com>
2015-07-08 21:02:36 +0200

    [Added bootstrap to charts]

--- Chart/graph.js
+++ Chart/graph.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 				var ctx = $("#mycanvas").get(0).getContext("2d");
-
+				
 				//pie chart data
 				//sum of values = 360
 				var data = [
@@ -26,4 +26,20 @@ $(document).ready(function(){
 
 				//draw
 				var piechart = new Chart(ctx).Pie(data);
+				
+				var buyerData = {
+					labels : ["January","February","March","April","May","June"],
+					datasets : [
+								{
+									fillColor : "rgba(172,194,132,0.4)",
+									strokeColor : "#ACC26D",
+									pointColor : "#fff",
+									pointStrokeColor : "#9DB86D",
+									data : [203,156,99,251,305,247]
+								}
+								]
+								}
+				var ctx2 = $("#line").get(0).getContext("2d");
+				new Chart(ctx2).Line(buyerData);
 			});
+
